c116c01cd5da423cb9d462525cdbb1e3cb1da0d1
Yeshi <nikolai.bjerke.sakariassen2@gmail.com>
2022-11-29 01:48:42 -0800

    updated temp cleaning script

--- internal/newEngine/scripts/cleanTemp.bat
+++ internal/newEngine/scripts/cleanTemp.bat
@@ -1,3 +1,3 @@
-if exist log.txt del log.txt
 if exist newEngineProject\temp\tempObject.yss del newEngineProject\temp\tempObject.yss
+if exist newEngineProject\temp\devConsoleCommand.yss del newEngineProject\temp\devConsoleCommand.yss
 exit /b 0
