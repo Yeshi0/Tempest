754bfc271837acf8e5fe039796df971d9cf55ade
Yeshi <nikolai.bjerke.sakariassen2@gmail.com>
2022-11-30 04:49:50 -0800

    added (temporary) update script

--- temp-update.bat
+++ temp-update.bat
@@ -2,20 +2,29 @@
 setLocal enableDelayedExpansion
 
 cls
-echo This will delete and redownload the latest files from GitHub.
-echo Enter "download" to confirm.
+echo.This will delete and redownload the latest files from GitHub.
+echo.Enter "download" to confirm.
 echo.
 set /p input=Confirm: 
 
 if NOT "!input!"=="download" (
-    cls
-    echo Cancelled.
-    pause
-    exit /b 0
+	cls
+	echo Cancelled.
+	pause
+	exit /b 0
 )
 
 cls
-echo Downloading file list...
+echo.Updating...
 
-for /f "delims=1 tokens=" %%a in ('curl -kL https://raw.githubusercontent.com/Yeshi0/Tempest/master/fileList ^2>nul') do echo.%%a
+for /f "tokens=1 delims=" %%a in ('curl -kL https://raw.githubusercontent.com/Yeshi0/Tempest/master/fileList 2^>nul') do (
+	echo.Updating "%%a"...
+	if "%%a"=="temp-update.bat" (
+		if exist %%a del %%a
+		curl -kL https://raw.githubusercontent.com/Yeshi0/Tempest/master/ -o %%a 2^>nul
+
+	) else echo.Cannot self-update, skipping... ^(ignore this message^)
+)
+
+echo.Done.
 pause
