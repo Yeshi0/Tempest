cd998ddb122f63f86b23b49e9fa81a73f4fab7a2
Yeshi <nikolai.bjerke.sakariassen2@gmail.com>
2022-12-02 01:59:13 -0800

    switchScene api now changes rtVar_result

--- internal/newEngine/scripts/switchScene.bat
+++ internal/newEngine/scripts/switchScene.bat
@@ -4,6 +4,10 @@ call newEngine\scripts\objectManager.bat killAll
 for /l %%a in (1,1,56) do set d%%a=                                                                                        
 if exist newEngineProject\scenes\%1\onLoad.yss (
 	call newEngine\scripts\scriptManager.bat start scenes\%1\onLoad.yss
+) else (
+	set rtVar_result=failed
+	exit /b 1
+)
 
-) else exit /b 1
+set rtVar_result=switchedScene
 exit /b 0
