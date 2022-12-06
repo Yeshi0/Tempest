37e9b52c2bffe41747256ff550eef664e03eaca2
Yeshi <nikolai.bjerke.sakariassen2@gmail.com>
2022-12-06 01:39:29 -0800

    changed window width, added empty framebuffer

--- NewEngineEditor/internal/scripts/main.bat
+++ NewEngineEditor/internal/scripts/main.bat
@@ -2,3 +2,15 @@
 setLocal enableDelayedExpansion
 
 call scripts\init.bat
+goto :main
+
+:halt
+pause >nul
+goto :halt
+
+:main
+for /l %%. in (1,1,2999) do (
+    <nul set /p=[1;1H
+    for /l %%a in (56,-1,1) do echo.!d%%a!
+)
+goto :main
--- NewEngineEditor/internal/scripts/readWriteTest.bat
+++ NewEngineEditor/internal/scripts/readWriteTest.bat
@@ -1,19 +1,19 @@
-if exist newEngineProject\readWriteTest.tmp del newEngineProject\readWriteTest.tmp
-if exist newEngineProject\readWriteTest.tmp (
+if exist readWriteTest.tmp del readWriteTest.tmp
+if exist readWriteTest.tmp (
 	echo.
 	echo Permission check for read/write failed.
 	call scripts\halt.bat
 )
 
-echo.readWriteTest>>newEngineProject\readWriteTest.tmp
-if NOT exist newEngineProject\readWriteTest.tmp (
+echo.readWriteTest>>readWriteTest.tmp
+if NOT exist readWriteTest.tmp (
 	echo.
 	echo Permission check for read/write failed.
 	call scripts\halt.bat
 )
 
-if exist newEngineProject\readWriteTest.tmp del newEngineProject\readWriteTest.tmp
-if exist newEngineProject\readWriteTest.tmp (
+if exist readWriteTest.tmp del readWriteTest.tmp
+if exist readWriteTest.tmp (
 	echo.
 	echo Permission check for read/write failed.
 	call scripts\halt.bat
